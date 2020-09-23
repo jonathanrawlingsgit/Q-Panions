9e4a2a3fc0a67ce7334515bfb2969e35be9eb0e1
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2020-09-23 10:21:03 -0700

    set up models, migration, schema

---
+++ db/migrate/20200923165827_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
