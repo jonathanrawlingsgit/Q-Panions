ecec35ea7b5e31c3b58db26f9329dc6387e68d35
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2020-09-28 08:06:21 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "/login" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
-  get "profile" => "users#profile"
+  # post "/login" => "sessions#create"
+  # delete "/logout" => "sessions#destroy"
+  # get "profile" => "users#profile"
   resources :users
 end
